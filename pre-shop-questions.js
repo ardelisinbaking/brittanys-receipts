167796522d4c607bcbc85aeca0d78f865960498d
Ardelis Yeung <oyu.ardelis@gmail.com>
2022-12-25 19:40:01 -0800

    adds file collect-answers.js; collectAnswers function becomes own module

--- pre-shop-questions.js
+++ pre-shop-questions.js
@@ -1,10 +1,4 @@
-//practice readline
-const readline = require("readline");
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const collectAnswers = require("./library/collect-answers")
 
 const questions = [
     "What pieces are you looking for?\n",
@@ -12,20 +6,9 @@ const questions = [
     "Are there any stores in particular you would like to shop?\n",
 ];
 
-const collectAnswers = (questions, done) => {
-    const answers = [];
-    const questionAnswered = (answer) => {
-        answers.push(answer);
-        answers.length < questions.length ?
-        rl.question(questions[answers.length], questionAnswered) : done(answers);
-    };
-    rl.question(questions[0], questionAnswered);
-};
-
 collectAnswers(questions, answers => {
     console.log("\nTo review our mission:");
     console.log(`We are looking for ${answers[0]} with a spend under ${answers[1]}.`);
     console.log(`First stop ${answers[2]}. Lets go shopping!`);
     process.exit();
 });
-//adds a semicolon and minor mistype
